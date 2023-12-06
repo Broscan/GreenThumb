5ffc9e990cd9f7cc8aaff8d472bf92621f2dc8db
Broscan <Calle992015@gmail.com>
2023-12-06 12:14:42 +0100

    almost finished with add button

--- GreenThumb/AddPlantWindow.xaml.cs
+++ GreenThumb/AddPlantWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using GreenThumb.Database;
+using GreenThumb.Models;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace GreenThumb
 {
@@ -7,12 +10,66 @@ namespace GreenThumb
     /// </summary>
     public partial class AddPlantWindow : Window
     {
+        List<string> instructions = new();
         public AddPlantWindow()
         {
             InitializeComponent();
         }
 
         private void btn_AddPlant(object sender, RoutedEventArgs e)
+        {
+
+            using (AppDbContext context = new())
+            {
+
+                ListViewItem selectedItem = (ListViewItem)lstInstructions.SelectedItem;
+
+
+                if (selectedItem != null)
+                {
+                    string plantName = txtPlant.Text;
+                    string plantDesc = txtDesc.Text;
+
+                    foreach (var instruction in instructions)
+                    {
+                        InstructionModel newInstruction = new()
+                        {
+                            Instruction = instruction,
+                        };
+                    }
+
+
+                    PlantModel newPlant = new()
+                    {
+                        PlantName = plantName,
+                        Description = plantDesc,
+
+                    };
+                }
+                else
+                {
+                    MessageBox.Show("Some of the inputs are empty!", "Warning");
+                }
+
+            }
+
+        }
+
+        private void btn_AddInstruction(object sender, RoutedEventArgs e)
+        {
+            if (txtInstructions != null)
+            {
+                string newInstruction = txtInstructions.Text;
+                instructions.Add(newInstruction);
+
+                foreach (string instruction in instructions)
+                {
+                    lstInstructions.Items.Add(instruction);
+                }
+            }
+        }
+
+        private void UpdateUI()
         {
 
         }
